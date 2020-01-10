006b733f2cc1c81120cd22f8c36920b2e1cce28d
Omar Dávila <omar@zinobe.com>
2020-01-10 10:42:48 -0500

    add new variables

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource aws_api_gateway_domain_name lambda_api_gateway_custom_domain {
+resource aws_api_gateway_domain_name api_gateway_custom_domain {
   domain_name     = var.domain_name
   regional_certificate_arn = var.certificate_arn
 
@@ -9,19 +9,19 @@ resource aws_api_gateway_domain_name lambda_api_gateway_custom_domain {
 
 resource aws_api_gateway_base_path_mapping base_path {
   api_id      = var.api_id
-  domain_name = aws_api_gateway_domain_name.lambda_api_gateway_custom_domain.domain_name
+  domain_name = aws_api_gateway_domain_name.api_gateway_custom_domain.domain_name
   base_path  = var.api_stage_name
   stage_name  = var.api_stage_name
 }
 
-resource aws_route53_record a {
-  type     = "A"
-  name     = aws_api_gateway_domain_name.lambda_api_gateway_custom_domain.domain_name
+resource aws_route53_record record {
+  type     = var.route53_record_type
+  name     = aws_api_gateway_domain_name.api_gateway_custom_domain.domain_name
   zone_id  = var.route53_zone_id
 
   alias {
-    evaluate_target_health = false
-    name                   = aws_api_gateway_domain_name.lambda_api_gateway_custom_domain.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.lambda_api_gateway_custom_domain.regional_zone_id
+    evaluate_target_health = var.route53_record_evaluate_target_health
+    name                   = aws_api_gateway_domain_name.api_gateway_custom_domain.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.api_gateway_custom_domain.regional_zone_id
   }
 }
